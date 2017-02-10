e09c0ac37d5d2d826d3785f18797f24c3683fc6d
Saiteja <saitejadidigam1@gmail.com>
2017-02-10 16:12:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article one',function (req,res)
+app.get('/article one', function (req,res)
 {
     res.send('article-one requested one will be served');
 });
 
-app.get('/article two',function (req,res)
+app.get('/article two', function (req,res)
 {
     res.send('article-two requested one will be served');
 });
 
-app.get('/article-three',function (req,res)
+app.get('/article-three', function (req,res)
 {
     res.send('article three requested one will be served');
 });
