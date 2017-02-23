9d1ae0ec5b1703cf18098e15360c0c9dc6dd2e0e
Saiteja <saitejadidigam1@gmail.com>
2017-02-23 20:51:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/article-one', function (req,res)
 {
-    res.send('article-one.html'));
+    res.send('article-one.html');
 });
 
 app.get('/article-two', function (req,res)
