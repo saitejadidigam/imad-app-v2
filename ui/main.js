3abe334d28913c1ecd26782c08d4841bcdc57748
Saiteja <saitejadidigam1@gmail.com>
2017-02-24 17:58:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML='new value';
 
 var img=document.getElementById('madi');
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveleft,100);
 };
