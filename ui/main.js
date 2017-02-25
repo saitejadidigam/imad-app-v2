c6bd141dfc97c82e084d93d0dfe4d4537078c394
Saiteja <saitejadidigam1@gmail.com>
2017-02-25 10:08:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,19 @@ button.onclick=function(){
     request.open('GET','http://saitejadidigam.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    //make a request to server and send name
+    //capture list of names and render it as a list
+    var names=['n1','n2','n3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'<li>';
+    }
+    var ul=document.getElementById('namelist);
+    ul.innerHTML=list;
+};
