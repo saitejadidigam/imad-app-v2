5bbf787b04e31aaa9412a3a3e8f6018806dbc747
Saiteja <saitejadidigam1@gmail.com>
2017-02-25 08:55:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,7 @@
-console.log('Loaded!');
-
-
-var element=document.getElementById('main text');
-element.innerHTML='new value';
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-};
-
-
-
-
-
-
-
 
 var button=document.getElementById('counter');
 
-counter=0;
+var counter=0;
 button.onclick=function(){
   counter=counter+1;
   var span=document.getElementById('count');
