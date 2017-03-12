72828eaded4e00d879bbd2438f8b26ffb5d32758
Saiteja <saitejadidigam1@gmail.com>
2017-03-12 10:22:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'saitejadidigam',
+  database: 'saitejadidigam',
+  port:'5432',
+   password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -70,6 +78,21 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+
+pool.query('SELECT *FROM test',function(err,result){
+   if(err)
+   {
+       res.status(500).send(err.toString());
+   }
+   else
+   {
+       res.send(JSON.stringify(result));
+   }
+});    
 });
 
 
